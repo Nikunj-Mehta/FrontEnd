c2144d0a670ccb1e125488798451e1f493c56921
Nikunj-Mehta <mehtanikunj11@gmail.com>
2024-10-24 20:01:51 +0530

    completed lesson 11 successfully with exercises.

---
+++ javascript-course/Practice Exercises/scripts/11x.js
@@ -0,0 +1,66 @@
+const todoList = JSON.parse(localStorage.getItem('todoList')) || [
+  {
+    name: 'make dinner',
+    dueDate: '2022-12-22'
+ },
+ { 
+    name: 'wash dishes',
+    dueDate: '2022-12-22'
+}];
+
+renderTodoList();
+
+function renderTodoList()
+{
+  let todoListHTML = '';
+
+  for(let i = 0; i < todoList.length; i++)
+  {
+    const todoObject = todoList[i];
+    // const name = todoObject.name;
+    // const dueDate = todoObject.dueDate;
+    const { name, dueDate } = todoObject;
+    const html = `
+      <div>${name}</div>
+      <div>${dueDate}</div> 
+      <button onclick="
+        todoList.splice(${i}, 1);
+        renderTodoList();
+        
+        saveToStorage();
+      " class="delete-todo-button">Delete</button>
+      `; // Generating HTML
+    todoListHTML += html;
+  }
+
+  document.querySelector('.js-todo-list')     // This line adds an element to js
+    .innerHTML = todoListHTML;
+}
+
+function addTodo()
+{
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+  
+  todoList.push({
+    // name: name,
+    // dueDate: dueDate
+    // Instead use short hand property
+    name,
+    dueDate
+  });
+
+  inputElement.value = '';
+
+  renderTodoList();
+
+  saveToStorage();
+}
+
+function saveToStorage()
+{
+  localStorage.setItem('todoList', JSON.stringify(todoList));
+}
