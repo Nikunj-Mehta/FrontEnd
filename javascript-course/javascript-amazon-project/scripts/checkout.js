2b1f86ffd17ca13729e2f5294a065b740116afee
Nikunj-Mehta <mehtanikunj11@gmail.com>
2024-12-23 14:10:10 +0530

    Delete products form HTML when clicking delete.

--- javascript-course/javascript-amazon-project/scripts/checkout.js
+++ javascript-course/javascript-amazon-project/scripts/checkout.js
@@ -1,6 +1,6 @@
 import {cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
-import {formatCurrency} from './utils/money.js';
+import {formatCurrency} from './utils/money.js'; 
 
 let cartSummaryHTML = '';
 
@@ -19,7 +19,7 @@ cart.forEach((cartItem) => {
 //  console.log(matchingProduct);
 
 cartSummaryHTML += ` 
-  <div class="cart-item-container">
+  <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">
       Delivery date: Tuesday, June 21
     </div>
@@ -107,6 +107,11 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
 //      console.log(productId);
       removeFromCart(productId);
 //      console.log(cart);
+
+      const container = document.querySelector(`.js-cart-item-container-${productId}`);
+//    console.log(container);  
+      // step 2: Use .remove() method to remove the element form cart
+      container.remove();
   });
 });
 
