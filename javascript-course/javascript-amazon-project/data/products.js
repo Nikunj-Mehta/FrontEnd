b2ac2cebb0a79823a819cc99bb78b3748d9afb76
Nikunj-Mehta <mehtanikunj11@gmail.com>
2025-01-10 16:28:42 +0530

    Use inheritance and polymorphism to add size chart

--- javascript-course/javascript-amazon-project/data/products.js
+++ javascript-course/javascript-amazon-project/data/products.js
@@ -34,7 +34,49 @@ class Products {
   getPrice() {
     return `$${formatCurrency(this.priceCents)}`;
   }
+
+  extraInfoHTML() {
+    return '';
+  }
 }
+
+class Clothing extends Products{
+  sizeChartLink;
+
+  constructor(productDetails) {
+    super(productDetails); // calls the constructor of parent class
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML() { // Method overriding.
+    // super.extraInfoHTML();
+    return `
+      <a href="${this.sizeChartLink}" target="_blank">Size chart</a>
+    `;
+  }
+}
+
+/*
+const tshirt = new Clothing({
+    id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    name: "Adults Plain Cotton T-Shirt - 2 Pack",
+    rating: {
+      stars: 4.5,
+      count: 56
+    },
+    priceCents: 799,
+    keywords: [
+      "tshirts",
+      "apparel",
+      "mens"
+    ],
+    type: "clothing", // Discriminator property
+    sizeChartLink: "images/clothing-size-chart.png"
+  });
+
+  console.log(tshirt);
+  console.log(tshirt.getPrice());
 
 /* 
 It was just an example
@@ -758,6 +800,11 @@ export const products = [
     ]
   }
 ].map((productDetails) => {  // map loops through an array 
+  
+  if(productDetails.type == 'clothing') {
+    return new Clothing(productDetails);
+  }
+
   return new Products(productDetails);  // it will take the first value save it in productDetails, run the function. (repeat)
 }); // we take a product transform it into objects(class) and then save it into new array
 
