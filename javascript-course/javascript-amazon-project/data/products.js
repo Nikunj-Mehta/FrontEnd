9efeebf8bc87f6b990bbb8655fc07043193be3a6
Nikunj-Mehta <mehtanikunj11@gmail.com>
2025-01-10 13:39:27 +0530

    Convert products into classes

--- javascript-course/javascript-amazon-project/data/products.js
+++ javascript-course/javascript-amazon-project/data/products.js
@@ -1,3 +1,5 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+
 export function getProduct(productId) {
   let matchingProduct;
 
@@ -9,6 +11,50 @@ export function getProduct(productId) {
   });
   return matchingProduct;
 }
+
+class Products {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStarsUrl() {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice() {
+    return `$${formatCurrency(this.priceCents)}`;
+  }
+}
+
+/* 
+It was just an example
+const product1 = new Products({
+  id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+  image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+  name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+  rating: {
+    stars: 4.5,
+    count: 87
+  },
+  priceCents: 1090,
+  keywords: [
+    "socks",
+    "sports",
+    "apparel"
+  ]
+});
+console.log(product1);
+*/
 
 export const products = [
   {
@@ -711,4 +757,8 @@ export const products = [
       "womens"
     ]
   }
-];
+].map((productDetails) => {  // map loops through an array 
+  return new Products(productDetails);  // it will take the first value save it in productDetails, run the function. (repeat)
+}); // we take a product transform it into objects(class) and then save it into new array
+
+// console.log(products);
